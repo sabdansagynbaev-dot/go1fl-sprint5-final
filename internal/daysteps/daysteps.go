7287a1863f9beac9946f17e631020580ea723f21
shabdan <sabdansagynbaev@gmail.com>
2026-02-15 09:43:29 +0500

    fix daysteps

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -12,49 +12,84 @@ type DaySteps struct {
 	// TODO: добавить поля
 	Steps int
 	Duration time.Duration
-	personaldata.Personal
+	Weight float64
+	Height float64
+
+	Personal personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
 	parts := strings.Split(datastring, ",")
-    if len(parts) != 2 {
-        return errors.New("invalid data string format")
-    }
 
-    steps, err := strconv.Atoi(parts[0])
-if err != nil {
- return err
-}
-if steps <= 0 {
- return errors.New("invalid steps")
-}
+ if len(parts) != 2 && len(parts) != 4 {
+  return errors.New("invalid data string format")
+ }
 
-duration, err := time.ParseDuration(parts[1])
-if err != nil {
- return err
-}
-if duration <= 0 {
- return errors.New("invalid duration")
-}
+ for _, p := range parts {
+  if strings.TrimSpace(p) != p {
+   return errors.New("invalid data string format")
+  }
+ }
 
-ds.Steps = steps
-ds.Duration = duration
-return nil
+ steps, err := strconv.Atoi(parts[0])
+ if err != nil || steps <= 0 {
+  return errors.New("invalid steps")
+ }
+
+ duration, err := time.ParseDuration(parts[1])
+ if err != nil || duration <= 0 {
+  return errors.New("invalid duration")
+ }
+
+ ds.Steps = steps
+ ds.Duration = duration
+
+ if len(parts) == 4 {
+  weightInt, err := strconv.Atoi(parts[2])
+  if err != nil || weightInt <= 0 {
+   return errors.New("invalid weight")
+  }
+
+  heightInt, err := strconv.Atoi(parts[3])
+  if err != nil || heightInt <= 0 {
+   return errors.New("invalid height")
+  }
+
+  ds.Weight = float64(weightInt)
+  ds.Height = float64(heightInt)
+  ds.Personal.Weight = ds.Weight
+  ds.Personal.Height = ds.Height
+ }
+
+ return nil
 }
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
-	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	 distance := spentenergy.Distance(ds.Steps, float64(ds.Personal.Height))
 
-    calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
-    if err != nil {
-        return "", err
-    }
+ calories, err := spentenergy.WalkingSpentCalories(
+  ds.Steps,
+  float64(ds.Personal.Weight),
+  float64(ds.Personal.Height),
+  ds.Duration,
+ )
+ if err != nil {
+  return "", err
+ }
 
-    info := fmt.Sprintf(
-        "Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
-        ds.Steps, distance, calories,
-    )
+ info := fmt.Sprintf(
+  "Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+  ds.Steps, distance, calories,
+ )
+ return info, nil
+}
 
-    return info, nil
+func (ds DaySteps) Print() {
+ info, err := ds.ActionInfo()
+ if err != nil {
+  fmt.Println(err)
+  return
+ }
+ fmt.Print(info)
 }
